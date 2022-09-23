6486aab8d76f063f81aba262891d7b9d50ba9f2b
mscerba <scerba4@gmail.com>
2022-09-23 15:13:55 +0200

    priprava pro nasazení na heroku

--- karateWebsite/settings.py
+++ karateWebsite/settings.py
@@ -1,3 +1,10 @@
+import os
+import django_heroku
+import dj_database_url
+from decouple import config
+
+
+
 """
 Django settings for karateWebsite project.
 
@@ -57,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'karateWebsite.urls'
@@ -122,7 +130,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+STATICFILES_STORAGE	= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
